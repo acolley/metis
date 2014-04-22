643f818f1af8f5ef7011c6b5c525818e93fe5371
Alasdair Colley <alnessy@hotmail.com>
2014-04-22 18:31:24 +0200

    adding support for uniquely identifying a component in the system

--- src/core/component.rs
+++ src/core/component.rs
@@ -1,4 +1,8 @@
 
+use collections::HashMap;
+use std::any::Any;
+use std::intrinsics::TypeId;
+
 pub trait Component {
 	// the component is a bit flag that uniquely
 	// identifies the component among other components
@@ -8,4 +12,70 @@ pub trait Component {
 	fn get_component_type(&self) -> u64;
 	// fn start(&mut self);
 	// fn update(&mut self);
+}
+
+#[deriving(Eq, Hash, Show)]
+pub struct ComponentType(u64);
+
+// TODO: could just override add to do bit shifting
+
+impl BitAnd<ComponentType, ComponentType> for ComponentType {
+    fn bitand(&self, rhs: &ComponentType) -> ComponentType {
+        let ComponentType(left) = *self;
+        let ComponentType(right) = *rhs;
+        ComponentType(left & right)
+    }
+}
+
+impl BitOr<ComponentType, ComponentType> for ComponentType {
+    fn bitor(&self, rhs: &ComponentType) -> ComponentType {
+        let ComponentType(left) = *self;
+        let ComponentType(right) = *rhs;
+        ComponentType(left | right)
+    }
+}
+
+impl BitXor<ComponentType, ComponentType> for ComponentType {
+    fn bitxor(&self, rhs: &ComponentType) -> ComponentType {
+        let ComponentType(left) = *self;
+        let ComponentType(right) = *rhs;
+        ComponentType(left ^ right)
+    }
+}
+
+impl Shl<int, ComponentType> for ComponentType {
+    fn shl(&self, rhs: &int) -> ComponentType {
+        let ComponentType(value) = *self;
+        ComponentType(value << *rhs)
+    }
+}
+
+impl Shr<int, ComponentType> for ComponentType {
+    fn shr(&self, rhs: &int) -> ComponentType {
+        let ComponentType(value) = *self;
+        ComponentType(value >> *rhs)
+    }
+}
+
+// TODO: change u64 references to ComponentType ones
+pub struct ComponentTypeManager {
+    current_id: ComponentType,
+    types: HashMap<TypeId, ComponentType>
+}
+
+impl ComponentTypeManager {
+    pub fn new() -> ComponentTypeManager {
+        ComponentTypeManager {
+            current_id: ComponentType(1u64),
+            types: HashMap::new()
+        }
+    }
+
+    pub fn get_type_for<T: Component + 'static>(&mut self) -> ComponentType {
+        let id = self.types.find_or_insert(TypeId::of::<T>(), self.current_id);
+        if self.current_id == *id {
+            self.current_id = self.current_id << 1;
+        }
+        *id
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-#![crate_id="engine#0.1"]
+#![crate_id="metis#0.1"]
 #![crate_type = "lib"]
 #![crate_type = "rlib"]
 #![crate_type = "dylib"]
 #![comment = "A game engine"]
 
 extern crate cgmath;
-extern crate glfw;
+//extern crate glfw;
 
 extern crate collections;
 
